2f3e680051ff9a2829582bff63b2fadaf3acecf2
Denis Hananein <i@zloylos.me>
2016-04-18 22:23:38 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+export default class BlaBla extends Component {
+    render() {
+        return (
+            <div>{'asdfasdfds'}</div>
+        );
+    }
+}
