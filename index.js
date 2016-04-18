0a900b93c15eb15420820682f53cd57de7c4358d
Denis Hananein <i@zloylos.me>
2016-04-18 22:27:22 +0300

    updated bla

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 import React, {Component} from 'react';
 
+const DEFAULT_TEST = 100;
+
 export default class BlaBla extends Component {
     static propTypes = {
-        bla: React.PropTypes.bool
+        bla: React.PropTypes.bool,
+        test: React.PropTypes.number
     }
 
     render() {
-        const {bla} = this.props;
+        const {bla, test} = this.props;
         return (
-            <div>{bla}</div>
+            <div>{bla ? test : DEFAULT_TEST}</div>
         );
     }
 }
