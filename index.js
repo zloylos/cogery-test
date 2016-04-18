bca3be41b8f22ac2566e5789e0b13b648b37298f
Denis Hananein <i@zloylos.me>
2016-04-18 22:24:44 +0300

    update

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import React, {Component} from 'react';
 
 export default class BlaBla extends Component {
+    static propTypes = {
+        bla: React.PropTypes.bool
+    }
+
     render() {
+        const {bla} = this.props;
         return (
-            <div>{'asdfasdfds'}</div>
+            <div>{bla}</div>
         );
     }
 }
