ab6bb4d6aab452a5bc537a723d21ef68f6594fb1
Denis Hananein <i@zloylos.me>
2016-11-21 22:29:27 +0300

    test

---
+++ index-2.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+
+const DEFAULT_TEST = 100;
+
+export default class BlaBla extends Component {
+    static propTypes = {
+        bla: React.PropTypes.bool,
+        test: React.PropTypes.number
+    }
+    static propTypes = {
+        bla: React.PropTypes.bool,
+        test: React.PropTypes.number
+    }
+    componentWillMount() {
+        this.setState({
+            wow: test > 500,
+            wowwow: bla && test > 900
+        });
+    }
+
+    render() {
+        const {bla, test} = this.props;
+        const {wow, wowwow} = this.state;
+        return (
+            <div>
+                {bla ? test : DEFAULT_TEST}
+                {wow ? <span>{'test'}</span> : null}
+            </div>
+        );
+    }
+}
