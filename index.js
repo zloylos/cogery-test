0ae32145a93dd0142db093418dcfde27e2f5a836
Denis Hananein <i@zloylos.me>
2017-06-03 16:59:43 +0300

    Up

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-import React, {Component} from 'react';
+import {Component} from 'react';
+import PropTypes from 'prop-types';
 
 export default class BlaBla extends Component {
+    static propTypes = {
+        text: PropTypes.string
+    }
+
+    static defaultProps = {
+        text: ''
+    }
+
     render() {
+        const {text} = this.props;
         return (
-            <div>{'asdfasdfds'}</div>
+            <div>{text}</div>
         );
     }
 }
